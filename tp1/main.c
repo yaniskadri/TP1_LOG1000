94724b2656126942001d15becf349b24fd03d8d1
Yanis Kadri <yanis.kadri@usa.com>
2019-09-22 18:39:41 -0400

    Modif commentaires de coequipier #1

--- tp1/main.c
+++ tp1/main.c
@@ -6,8 +6,8 @@
 
 /*
  * argc : Number of parameters passed to the program. The value is almost always one, for the program name.
- * argv : A list of char* strings. One a proper call, the first char* is the program name, the second char* is the value for which we want the root, and the third char* is the number of iteration we want for the Newton approximation of the root. 
- * return : Zero if the program worked allright, or the error code : 1 means not enought arguments were given. 2 means that the value to root is invalid. 3 means that the number of iterations is invalid.
+ * argv : A list of char* strings. One a proper call, the first char* is the program name, the second char* is the value for which we want the root, and the third char* is the number of iteration we want for the Newton approximation of the root.
+ * return : Zero if the PROGRAM worked allright, or the error code : 1 means not enought arguments were given. 2 means that the value to root is invalid. 3 means that the number of iterations is invalid.
  */
 int main(int argc, char** argv) {
 
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
 
 	// Print out the initial values.
 	printf("Compute the root value of %f\n", value_to_root);
-      	printf("With %d iterations\n", iterations);	
+      	printf("With %d iterations\n", iterations);
 
 	// Perform the Newton approximation of the root.
 	float root = newton(value_to_root, iterations);
