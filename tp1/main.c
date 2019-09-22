7dbd11c154edaa54c735d0f9fabfb166c84a4f46
Yanis Kadri <yanis.kadri@usa.com>
2019-09-22 18:41:24 -0400

    Modification par le coequipier #2

--- tp1/main.c
+++ tp1/main.c
@@ -7,7 +7,7 @@
 /*
  * argc : Number of parameters passed to the program. The value is almost always one, for the program name.
  * argv : A list of char* strings. One a proper call, the first char* is the program name, the second char* is the value for which we want the root, and the third char* is the number of iteration we want for the Newton approximation of the root.
- * return : Zero if the PROGRAM worked allright, or the error code : 1 means not enought arguments were given. 2 means that the value to root is invalid. 3 means that the number of iterations is invalid.
+ * return : Zero if the program worked allright, or the ERROR code : 1 means not enought arguments were given. 2 means that the value to root is invalid. 3 means that the number of iterations is invalid.
  */
 int main(int argc, char** argv) {
 
